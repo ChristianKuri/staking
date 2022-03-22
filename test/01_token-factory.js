a7d0f1bb0f270edb31582014335730388a71d036
Christian Kuri <christian.kuri.martinez@gmail.com>
2022-03-21 20:26:26 -0600

    format js

--- test/01_token-factory.js
+++ test/01_token-factory.js
@@ -1,26 +1,26 @@
-var ERC20Factory = artifacts.require("ERC20Factory.sol");
-var MockERC20 = artifacts.require("MockERC20.sol");
+var ERC20Factory = artifacts.require('ERC20Factory.sol')
+var MockERC20 = artifacts.require('MockERC20.sol')
 
-contract("Token Factory", async accounts => {
+contract('Token Factory', async (accounts) => {
   it('creates tokens', async () => {
-    const erc20factory = await ERC20Factory.new();
+    const erc20factory = await ERC20Factory.new()
 
-    await erc20factory.createToken("Basic Token", "BT");
-    await erc20factory.createToken("LP BT-BNB", "LPToken");
+    await erc20factory.createToken('Basic Token', 'BT')
+    await erc20factory.createToken('LP BT-BNB', 'LPToken')
 
-    const tokens = await erc20factory.getTokens();
+    const tokens = await erc20factory.getTokens()
 
-    assert(tokens.length === 2);
+    assert(tokens.length === 2)
 
-    const lpToken = await MockERC20.at(tokens[1]);
+    const lpToken = await MockERC20.at(tokens[1])
 
-    await lpToken.mint(accounts[1], web3.utils.toWei("1000000"));
-    await lpToken.mint(accounts[2], web3.utils.toWei("1000000"));
+    await lpToken.mint(accounts[1], web3.utils.toWei('1000000'))
+    await lpToken.mint(accounts[2], web3.utils.toWei('1000000'))
 
-    depositToken = tokens[1];
-    rewardToken = tokens[0];
+    depositToken = tokens[1]
+    rewardToken = tokens[0]
 
-    assert((await lpToken.balanceOf(accounts[1])).toString() === web3.utils.toWei("1000000"));
-    assert((await lpToken.balanceOf(accounts[2])).toString() === web3.utils.toWei("1000000"));
-  });
-});
+    assert((await lpToken.balanceOf(accounts[1])).toString() === web3.utils.toWei('1000000'))
+    assert((await lpToken.balanceOf(accounts[2])).toString() === web3.utils.toWei('1000000'))
+  })
+})
